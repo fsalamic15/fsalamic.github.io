b961e2b891ff4725b5bee7253a6297083f51b076
pangara <priya.angara@gov.bc.ca>
2017-10-20 13:46:29 -0700

    add readme to scripts

--- scripts/markdown-to-folders/chop_md.py
+++ scripts/markdown-to-folders/chop_md.py
@@ -15,6 +15,14 @@ def markdown_to_eguide(markdown):
         PAGES = "./_pages"
         if not os.path.exists(PAGES):
             os.makedirs(PAGES)
+        indexFile = open(PAGES+"/index.md", "w")
+        indexFile.write("---\n")
+        indexFile.write("permalink: /\n")
+        indexFile.write("title: Title\n")
+        indexFile.write("navtitle: Navigation Title\n")
+        indexFile.write("---\n")
+        indexFile.write("### content required\n")
+        indexFile.close()
         for lineIndex in range(len(LINES)):
             line = LINES[lineIndex]
             # If it is the heading,
@@ -34,7 +42,7 @@ def markdown_to_eguide(markdown):
                 lineIndex += 1
                 line = LINES[lineIndex]
                 # Iterate over the text in the section description and add it to the new md file
-                while not HEADING_PATTERN.match(line) and not SUBHEADING_PATTERN.match(line):
+                while not HEADING_PATTERN.match(line) and not SUBHEADING_PATTERN.match(line) and not SUBSUBHEADING_PATTERN.match(line):
                     mainFile.write(line+ "\n")
                     if lineIndex < len(LINES):
                         lineIndex += 1
@@ -44,17 +52,20 @@ def markdown_to_eguide(markdown):
             # Inside each of the folders make md files for each of the
             # subsections and fill in content
             if SUBHEADING_PATTERN.match(line):
+                subDirFileName = line[3:]
                 subFileName = line[3:]
-                subFile = open(PAGES+"/"+dirName + "/" + subFileName + ".md", "w")
+                subFile = open(PAGES +"/"+ dirName + "/" + subFileName + ".md", "w")
                 subFile.write("---\n")
                 subFile.write("title: " + dirName + "\n")
                 subFile.write("navtitle: " + subFileName +"\n")
                 subFile.write("---\n")
                 subFile.write(line + "\n")
+                if not os.path.exists(PAGES+"/"+dirName+"/"+subDirFileName):
+                    os.makedirs(PAGES+"/"+dirName+"/"+subDirFileName)
                 lineIndex += 1
                 if lineIndex < len(LINES):
                     line = LINES[lineIndex]
-                    while not HEADING_PATTERN.match(line) and not SUBHEADING_PATTERN.match(line):
+                    while not HEADING_PATTERN.match(line) and not SUBHEADING_PATTERN.match(line) and not SUBSUBHEADING_PATTERN.match(line):
                         subFile.write(line +"\n")
                         if SUBSUBHEADING_PATTERN.match(line):
                             subFile.write("---\n")
@@ -65,6 +76,29 @@ def markdown_to_eguide(markdown):
                             break
                 else:
                     break
+            if SUBSUBHEADING_PATTERN.match(line):
+                subSubFileName = line[4:]
+                subSubFile = open(PAGES + "/" + dirName + "/" + subDirFileName + "/" + subSubFileName + ".md", "w")
+                subSubFile.write("---\n")
+                subSubFile.write("title: " + subFileName + "\n")
+                subSubFile.write("navtitle: " + subSubFileName +"\n")
+                subSubFile.write("---\n")
+                subSubFile.write(line + "\n")
+                lineIndex += 1
+                if lineIndex < len(LINES):
+                    line = LINES[lineIndex]
+                    while not HEADING_PATTERN.match(line) and not SUBHEADING_PATTERN.match(line) and not SUBSUBHEADING_PATTERN.match(line):
+                        subSubFile.write(line +"\n")
+                        if SUBSUBHEADING_PATTERN.match(line):
+                            subSubFile.write("---\n")
+                        lineIndex += 1
+                        if lineIndex < len(LINES):
+                            line = LINES[lineIndex]
+                        else:
+                            break
+                else:
+                    break
+
     mdFile.close()
 
 if __name__ == '__main__':
